7dfa5e99ea45448118b6df6ff9970247c38ce9b6
unknown <yogevcohen@gmail.com>
2017-10-13 11:22:58 +0300

    commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const request = require('request');
+
+
+getAddress = (address, callback) => {
+    var encodedAddress = encodeURIComponent(address);
+    const client_Id = '&client_id=3092nxybyb0otqw18e8nh5nty';
+    request({
+        url: `https://api.airbnb.com/v2/search_results?location=${encodedAddress}${client_Id}&_limit=1&_offset=1`,
+        json: true
+    }, (error, response, body) => {
+
+
+        if (response.statusMessage === "OK") {
+            callback(undefined, {
+                search_results: body.search_results,
+                meta_data: body.meta_data,
+            });
+        } else {
+            callback({
+                error_message:body.error_message,
+                error_details:body.error_details,
+            });
+            return;
+        }
+
+    });
+}
+
+getAddress('Amsterdam, Netherlands', (errorMessage, results) => {
+    if (errorMessage) {
+        console.log(errorMessage);
+    } else {
+        console.log(JSON.stringify(results, undefined, 2));
+    }
+});
