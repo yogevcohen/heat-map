1567320a43b2248df98a1de39ab7a92019a8f14f
unknown <yogevcohen@gmail.com>
2017-10-14 17:57:02 +0300

    amsterdam

--- app.js
+++ app.js
@@ -10,36 +10,36 @@ var app = express();
 const port = process.env.PORT || 3000;
 
 
-hbs.registerPartials(__dirname + '/views/partials');
-app.set('view engine', 'hbs');
+// hbs.registerPartials(__dirname + '/views/partials');
+// app.set('view engine', 'hbs');
 
-app.use(express.static(__dirname + '/public'));
+// app.use(express.static(__dirname + '/public'));
 
-hbs.registerHelper('getCurrentYear', () => {
-    return new Date().getFullYear();
-});
+// hbs.registerHelper('getCurrentYear', () => {
+//     return new Date().getFullYear();
+// });
 
-hbs.registerHelper('getMap', (fileName) => {
-    var scr = `<script> function init(){initMap("${fileName}");} </script>\
-    <script async defer src="https://maps.googleapis.com/maps/api/js?key=${config.googleMapAPi}&libraries=visualization&callback=init"></script>`
-    return scr;
-});
+// hbs.registerHelper('getMap', (fileName) => {
+//     var scr = `<script> function init(){initMap("${fileName}");} </script>\
+//     <script async defer src="https://maps.googleapis.com/maps/api/js?key=${config.googleMapAPi}&libraries=visualization&callback=init"></script>`
+//     return scr;
+// });
 
-hbs.registerHelper('getHeatMapFiles', () => {
-    var menu = '';
-    fs.readdirSync(__dirname).filter((file) => file.indexOf('heat') > 0).forEach(file => {
-        menu += `<a href='/viewheatmap/${file}'>${file.slice(0, file.indexOf('heat') - 1)}</a>`;
-    })
+// hbs.registerHelper('getHeatMapFiles', () => {
+//     var menu = '';
+//     fs.readdirSync(__dirname).filter((file) => file.indexOf('heat') > 0).forEach(file => {
+//         menu += `<a href='/viewheatmap/${file}'>${file.slice(0, file.indexOf('heat') - 1)}</a>`;
+//     })
 
-    return menu;
-});
+//     return menu;
+// });
 
-app.get('/', (req, res) => {
-    res.send('dsds');
-    // res.render('home.hbs', {
-    //     pageTitle: 'Home',
-    // });
-});
+// app.get('/', (req, res) => {
+//     //res.send('dsds');
+//     res.render('home.hbs', {
+//         pageTitle: 'Home',
+//     });
+// });
 
 app.get('/viewheatmap/:file', (req, res) => {
     res.render('heatmap.hbs', {
@@ -58,46 +58,46 @@ app.use((req, res, next) => {
 
 
 
-app.get('/files/:city', async (req, res) => {
-    try {
-        var obj = JSON.parse(fs.readFileSync(__dirname + `/${req.params.city}`, 'utf8'));
-        res.send(obj);
-    }
-    catch (e) {
-        console.log(e);
-    }
+// app.get('/files/:city', async (req, res) => {
+//     try {
+//         var obj = JSON.parse(fs.readFileSync(__dirname + `/${req.params.city}`, 'utf8'));
+//         res.send(obj);
+//     }
+//     catch (e) {
+//         console.log(e);
+//     }
 
-});
+// });
 
 
-app.get('/createheatmap/:city', async (req, res) => {
-    console.log(req.params);
+// app.get('/createheatmap/:city', async (req, res) => {
+//     console.log(req.params);
 
-    if (!req.params.city) {
-        console.log('City was not provided.');
-        res.send('Please provide city.');
-        return;
-    }
-    var city = req.params.city;
+//     if (!req.params.city) {
+//         console.log('City was not provided.');
+//         res.send('Please provide city.');
+//         return;
+//     }
+//     var city = req.params.city;
    
-    res.render('building-heat-map.hbs', {
-        city: city,
-    });
+//     res.render('building-heat-map.hbs', {
+//         city: city,
+//     });
 
-    var allListing = await listingManager.getAllListings(city);
-    calendarManager.getAllCalendarDays(allListing).then(
-        (values) => {
-            demandCalculator.calcHeatOnListings(values).then((listingWithHeat) => {
-                if (fs.existsSync(__dirname + `${city}.heat`)) {
-                    fs.unlinkSync(`${city}.heat`);
-                }
-                fs.appendFile(`${city}.heat`, JSON.stringify(listingWithHeat, undefined, 2), (err) => {
-                    console.log(err);
-                });
-            });
+//     var allListing = await listingManager.getAllListings(city);
+//     calendarManager.getAllCalendarDays(allListing).then(
+//         (values) => {
+//             demandCalculator.calcHeatOnListings(values).then((listingWithHeat) => {
+//                 if (fs.existsSync(__dirname + `${city}.heat`)) {
+//                     fs.unlinkSync(`${city}.heat`);
+//                 }
+//                 fs.appendFile(`${city}.heat`, JSON.stringify(listingWithHeat, undefined, 2), (err) => {
+//                     console.log(err);
+//                 });
+//             });
 
-        });
-});
+//         });
+// });
 
 app.listen(port, () => {
     console.log(`Server up ${port}`);
